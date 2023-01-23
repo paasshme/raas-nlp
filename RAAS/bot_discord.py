a6b6cd01e496ee638476d1a05ff2914ff3e9caf1
JacquesMironneau <lowneonema@gmail.com>
2023-01-23 10:36:34 +0100

    v1.2: first suppot for pronominal verbs

--- RAAS/bot_discord.py
+++ RAAS/bot_discord.py
@@ -9,6 +9,21 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
+async def handleSpecialMessages(message):
+    if message.content == "!ping":
+        await message.channel.send("pong uwu")
+        return True
+    if "cr7" in message.content:
+        # send gif
+        await message.channel.send("https://tenor.com/fr/view/siuu-gif-23749474")
+        return True
+    if "messi" in message.content:
+        await message.channel.send("cr7 > messi")
+        return True
+    return False
+
+        
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -19,9 +34,17 @@ async def on_message(message):
         # add reaction to own message heart
         await message.add_reaction("❤️")
         return
+    
+    print("Received message: ", message.content)
+    if await handleSpecialMessages(message):
+        return
+
     ratio_phrase = phrase_to_ratio(message.content)
     if ratio_phrase != "":
         await message.channel.send(ratio_phrase)
+    #else:
+    #    # post the looser reaction
+    #    await message.add_reaction("👎")
 
 @client.event
 async def on_message_edit(before,after):
--- RAAS/ratio.py
+++ RAAS/ratio.py
@@ -8,9 +8,11 @@ load_dotenv()
 nlp = spacy.load(os.getenv("FR_MODEL"))
 default_conjugator = mlconjug3.Conjugator(language='fr')
 
+pronoms_reflechis = ["me", "te", "se", "nous", "vous", "m'", "t'", "s'"]
 print("Init lemma read")
 data = {}
-with open(os.getenv("FR_LEMMA_PATH")) as f:
+# join cwd with env var
+with open(os.path.join(os.getcwd(), os.getenv("LEMMA_FILE")), "r", encoding="utf-8") as f:
     for line in f.readlines():
         splitLine = line.split('\t')
         data[splitLine[1].rstrip()] = splitLine[0]
@@ -29,18 +31,20 @@ def phrase_to_ratio(phrase):
     phrase = phrase.replace("?", "")
 
     doc = nlp(phrase)
-    print([token.pos_ for token in doc])
+    print([(token.pos_, token.text) for token in doc])
     verb_token = None
     i = 0
     b = 0
     docElemToPop = 0
     
+    # find the pronoum
     for token in doc:
         if token.pos_ == "PRON":
             break
         b+=1 + len(token.text)
         docElemToPop += 1
 
+    # find the verb (or aux)
     for token in doc:
         if token.pos_ == "AUX":
             verb_token = token
@@ -49,6 +53,12 @@ def phrase_to_ratio(phrase):
             verb_token = token
             break
         i+=1
+
+    # if the verb is pronominal
+    if verb_token.text in pronoms_reflechis:
+        verb_token = doc[i+1]
+        i+=1
+    
 
 
     phrase = phrase[b:]
@@ -59,8 +69,12 @@ def phrase_to_ratio(phrase):
         try:
             conjugated_verb =  default_conjugator.conjugate(verb_token.lemma_).conjug_info['Indicatif']['Présent']['3s']
         except:
-            verb = data[verb_token.text]
-            conjugated_verb =  default_conjugator.conjugate(verb).conjug_info['Indicatif']['Présent']['3s']
+            if verb_token.text in data:
+                verb = data[verb_token.text]
+                conjugated_verb =  default_conjugator.conjugate(verb).conjug_info['Indicatif']['Présent']['3s']
+            else:
+                print("no lemma found for " + verb_token.text)
+                conjugated_verb = verb_token.text
 
         print(f"p>>>  '{phrase}'")
         if "j'" in phrase or "J'" in phrase:
@@ -78,6 +92,8 @@ def phrase_to_ratio(phrase):
         phrase = phrase.replace("Je ","il ")
         phrase = phrase.replace("J'","il ")
         phrase = phrase.replace(" j'","il ")
+        phrase = phrase.replace(" m'"," s'")
+        phrase = phrase.replace(" me"," se")
         phrase = phrase.rstrip()
 
     res = f"Et ce ratio {phrase} ???"
@@ -133,3 +149,10 @@ if __name__ == "__main__":
 
     # Test 15
     assert phrase_to_ratio("Et oui, hier j'ai fait du jus") == "Et ce ratio il a fait du jus ???"
+
+    
+    # Test 16: pronominaux
+    assert phrase_to_ratio("Je te déteste") == "Et ce ratio il te déteste ???"
+    assert phrase_to_ratio("Je te mange fort") == "Et ce ratio il te mange fort ???"
+    #assert phrase_to_ratio("Je m'en fou") == "Et ce ratio il s'en fou ???"
+    assert phrase_to_ratio("Je me vois pas faire ça") == "Et ce ratio il se voit pas faire ça ???"
